a3c28086aa8d3b988cfba5f5cce64c717d0a518c
lynn <lynn2016nz@gmail.com>
2017-07-07 10:21:21 +1200

    get all jobIDs of search result

--- seek_job.py
+++ seek_job.py
@@ -7,8 +7,9 @@ from html.parser import HTMLParser
 
 # regular exp.
 def findResultNum(html):
-	jobList = re.findall('<span class="_2XFDIKN" data-automation="totalJobsCount" data-reactid="\d*">(.*?)</span>',html)
-	return jobList
+	# jobList = re.findall('<span class="_2XFDIKN" data-automation="totalJobsCount" data-reactid="\d*">(.*?)</span>',html)
+	jobNum = re.findall('"totalCount":(.*?),',html)
+	return jobNum
 
 # find the result of job numbers
 def getTotalPage(website):
@@ -20,7 +21,7 @@ def getTotalPage(website):
 		jobNum = result[0]
 		# print(jobNum)
 		totalPage = int((int(jobNum)-1)/20)+1
-		print(totalPage)
+		# print(totalPage)
 	return totalPage
 
 #find all the links of jobs in some page
@@ -29,33 +30,46 @@ def getJobLinks(website):
 	contents = ' '.join(html.content.decode('utf-8').split())
 	html_parser = HTMLParser()
 	originContent = html_parser.unescape(contents)
-	file = open ('page.txt','w')
-	file.write(str(html.content))
-	file.flush()
-	file.close()
+	# file = open ('page.txt','w')
+	# file.write(str(html.content))
+	# file.flush()
+	# file.close()
+	listJobIds = re.findall('"jobIds":\[(.*?)\],',originContent)
+	# print(len(listJobIds))
+	jobIds = listJobIds[0]
+	# print(jobIds)
+	jobSet = jobIds.split(',')
+	return jobSet
 	# print(re.findall(r'"(\/job\/.*?)"',originContent))
-	jobLinks = re.findall(r'"(\/job\/.*?)"',originContent)
-	print(len(jobLinks))
-	file = open ('joblinks.txt','w')
-	for jobLink in jobLinks:
-		file.write(jobLink + "\n")
-
-	file.flush()
-	file.close()
+	# jobLinks = re.findall(r'"(\/job\/.*?)"',originContent)
+	# jobLinks = re.findall(r'"https://www.seek.co.nz/job/(.*?)"', originContent)
+	# jobSet = set(jobLinks)
 
 # website address of online jobs
 basicWebsite = "https://www.seek.co.nz/jobs-in-information-communication-technology/testing-quality-assurance/in-All-New-Zealand"
+# website of different page
 websitePrifix = "https://www.seek.co.nz/jobs-in-information-communication-technology/testing-quality-assurance/in-All-New-Zealand?page="
-basicPrifix = "https://www.seek.co.nz"
+# prifix + jobID to locate the content of each job
+basicPrifix = "https://www.seek.co.nz/job/"
 
 if __name__ == '__main__':
+	#get the total num of search results
 	pageNum = getTotalPage(basicWebsite)
+
 	page = 1
-	while(page<=1):
+	file = open ('joblinks.txt','w')
+	while(page<=pageNum):
 		site = websitePrifix + str(page)
-		# print(site)
-		getJobLinks(site)
+		# get the job id in every page
+		jobSet=getJobLinks(site)
+		for jobLink in jobSet:
+			# write them into file
+			file.write(jobLink + "\n")
+
 		page += 1
+
+	file.flush()
+	file.close()
 
 
 
