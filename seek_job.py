a05848f612eb2a30a9e58faca011dee742f869b2
lynn <lynn2016nz@gmail.com>
2017-07-07 11:52:09 +1200

    adjust

--- seek_job.py
+++ seek_job.py
@@ -14,22 +14,19 @@ def findResultNum(html):
 # find the result of job numbers
 def getTotalPage(website):
 	totalPage = 0
-	html = requests.get(website)
-	contents = html.content.decode('utf-8')
-	result = findResultNum(contents)
-	if len(result)>0:
-		jobNum = result[0]
-		# print(jobNum)
-		totalPage = int((int(jobNum)-1)/20)+1
+	originContent = getWebsiteContent(website)
+	result = findResultNum(originContent)
+	if len(result) == 0:
+		return totalPage
+	jobNum = result[0]
+	# print(jobNum)
+	totalPage = int((int(jobNum)-1)/20)+1
 		# print(totalPage)
 	return totalPage
 
 #find all the links of jobs in some page
 def getJobLinks(website):
-	html = requests.get(website)
-	contents = ' '.join(html.content.decode('utf-8').split())
-	html_parser = HTMLParser()
-	originContent = html_parser.unescape(contents)
+	originContent = getWebsiteContent(website)
 	# file = open ('page.txt','w')
 	# file.write(str(html.content))
 	# file.flush()
@@ -45,6 +42,22 @@ def getJobLinks(website):
 	# jobLinks = re.findall(r'"https://www.seek.co.nz/job/(.*?)"', originContent)
 	# jobSet = set(jobLinks)
 
+def getWebsiteContent(website):
+	html = requests.get(website)
+	contents = ' '.join(html.content.decode('utf-8').split())
+	html_parser = HTMLParser()
+	originContent = html_parser.unescape(contents)
+	return originContent
+
+
+def getTitle(html):
+	title = re.findall('<h1 class="jobtitle">(.*?)</h1>',html)
+	return title
+
+def getJobContent(html):
+	content = re.findall('<div class="templatetext">(.*?)<div class="details">[.*?]</div></div>',html)
+	return content
+
 # website address of online jobs
 basicWebsite = "https://www.seek.co.nz/jobs-in-information-communication-technology/testing-quality-assurance/in-All-New-Zealand"
 # website of different page
@@ -55,7 +68,7 @@ basicPrifix = "https://www.seek.co.nz/job/"
 if __name__ == '__main__':
 	#get the total num of search results
 	pageNum = getTotalPage(basicWebsite)
-
+	pageNum = 1
 	page = 1
 	file = open ('joblinks.txt','w')
 	while(page<=pageNum):
@@ -63,6 +76,15 @@ if __name__ == '__main__':
 		# get the job id in every page
 		jobSet=getJobLinks(site)
 		for jobLink in jobSet:
+			jobAddress = basicPrifix + jobLink
+			pageContent = getWebsiteContent(jobAddress)
+			strTitle = getTitle(pageContent)
+			if len(strTitle) > 0:
+				title = str(strTitle[0]) + "(" + str(jobLink) + ")"
+				print(title)
+
+			jobContent = getJobContent(pageContent)
+			print(jobContent)
 			# write them into file
 			file.write(jobLink + "\n")
 
